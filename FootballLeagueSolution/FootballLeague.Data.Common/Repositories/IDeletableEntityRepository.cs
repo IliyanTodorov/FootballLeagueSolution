9e9a84db325db2ce58b139419854a8229d3be2e1
IliyanTodorov <iliyan.tdrv@gmail.com>
2025-03-02 12:51:02 +0200

    add repository architecture

---
+++ FootballLeagueSolution/FootballLeague.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace FootballLeague.Data.Common.Repositories
+{
+    using System.Linq;
+    using FootballLeague.Data.Common.Models;
+    
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
